bdc2eef20d8c831da00a992200761399cf5548f3
DESKTOP-34ARMFG\shivalik <harshitmaheshwari265@gmail.com>
2022-09-17 19:53:50 +0530

    commit code

---
+++ public/assets/js/main.js
@@ -0,0 +1,299 @@
+/*  ---------------------------------------------------
+    Template Name: Ogani
+    Description:  Ogani eCommerce  HTML Template
+    Author: Colorlib
+    Author URI: https://colorlib.com
+    Version: 1.0
+    Created: Colorlib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+        /*------------------
+            Gallery filter
+        --------------------*/
+        $('.featured__controls li').on('click', function () {
+            $('.featured__controls li').removeClass('active');
+            $(this).addClass('active');
+        });
+        if ($('.featured__filter').length > 0) {
+            var containerEl = document.querySelector('.featured__filter');
+            var mixer = mixitup(containerEl);
+        }
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Humberger Menu
+    $(".humberger__open").on('click', function () {
+        $(".humberger__menu__wrapper").addClass("show__humberger__menu__wrapper");
+        $(".humberger__menu__overlay").addClass("active");
+        $("body").addClass("over_hid");
+    });
+
+    $(".humberger__menu__overlay").on('click', function () {
+        $(".humberger__menu__wrapper").removeClass("show__humberger__menu__wrapper");
+        $(".humberger__menu__overlay").removeClass("active");
+        $("body").removeClass("over_hid");
+    });
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*-----------------------
+        Categories Slider
+    ------------------------*/
+    $(".categories__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+            0: {
+                items: 1,
+            },
+
+            480: {
+                items: 2,
+            },
+
+            768: {
+                items: 3,
+            },
+
+            992: {
+                items: 4,
+            }
+        }
+    });
+
+
+    $('.hero__categories__all').on('click', function(){
+        $('.hero__categories ul').slideToggle(400);
+    });
+
+    /*--------------------------
+        Latest Product Slider
+    ----------------------------*/
+    $(".latest-product__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: false,
+        nav: true,
+        navText: ["<span class='fa fa-angle-left'><span/>", "<span class='fa fa-angle-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*-----------------------------
+        Product Discount Slider
+    -------------------------------*/
+    $(".product__discount__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 3,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+
+            320: {
+                items: 1,
+            },
+
+            480: {
+                items:2,
+            },
+
+            768: {
+                items:2,
+            },
+
+            992: {
+                items:3,
+            }
+        }
+    });
+
+    /*---------------------------------
+        Product Details Pic Slider
+    ----------------------------------*/
+    $(".product__details__pic__slider").owlCarousel({
+        loop: true,
+        margin: 20,
+        items: 4,
+        dots: true,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true
+    });
+
+    /*-----------------------
+		Price Range Slider
+	------------------------ */
+    var rangeSlider = $(".price-range"),
+        minamount = $("#minamount"),
+        maxamount = $("#maxamount"),
+        minPrice = rangeSlider.data('min'),
+        maxPrice = rangeSlider.data('max');
+    rangeSlider.slider({
+        range: true,
+        min: minPrice,
+        max: maxPrice,
+        values: [minPrice, maxPrice],
+        slide: function (event, ui) {
+            minamount.val('$' + ui.values[0]);
+            maxamount.val('$' + ui.values[1]);
+        }
+    });
+    minamount.val('$' + rangeSlider.slider("values", 0));
+    maxamount.val('$' + rangeSlider.slider("values", 1));
+
+    /*--------------------------
+        Select
+    ----------------------------*/
+    $("select").niceSelect();
+
+    /*------------------
+		Single Product
+	--------------------*/
+    $('.product__details__pic__slider img').on('click', function () {
+
+        var imgurl = $(this).data('imgbigurl');
+        var bigImg = $('.drift-demo-trigger').attr('src');
+        if (imgurl != bigImg) {
+            $('.drift-demo-trigger').attr({
+                src: imgurl
+            });
+        }
+    });
+
+    /*-------------------
+		Quantity change
+	--------------------- */
+    var proQty = $('.pro-qty');
+    proQty.prepend('<span class="dec qtybtn">-</span>');
+    proQty.append('<span class="inc qtybtn">+</span>');
+    proQty.on('click', '.qtybtn', function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find('input').val();
+        if ($button.hasClass('inc')) {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 0) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 0;
+            }
+        }
+        $button.parent().find('input').val(newVal);
+    });
+
+})(jQuery);
+/*cart*/
+// Get the modal
+var modal = document.getElementById("myModal");
+                                
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+ const loginText = document.querySelector(".title-text .login");
+      const loginForm = document.querySelector("form.login");
+      const loginBtn = document.querySelector("label.login");
+      const signupBtn = document.querySelector("label.signup");
+      const signupLink = document.querySelector("form .signup-link a");
+      signupBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "-50%";
+        loginText.style.marginLeft = "-50%";
+      });
+      loginBtn.onclick = (()=>{
+        loginForm.style.marginLeft = "0%";
+        loginText.style.marginLeft = "0%";
+      });
+      signupLink.onclick = (()=>{
+        signupBtn.click();
+        return false;
+      });
+
+      function myFunction() {
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput1");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+          td = tr[i].getElementsByTagName("td")[0];
+          if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+              tr[i].style.display = "";
+            } else {
+              tr[i].style.display = "none";
+            }
+          }       
+        }
+      }
+      /*shop-deatails*/
+      var demoTrigger = document.querySelector('.demo-trigger');
+
+    new Drift(demoTrigger, {
+      paneContainer: document.querySelector('.detail'),
+      inlinePane: 900,
+      inlineOffsetY: -85,
+      containInline: true,
+      sourceAttribute: 'href'
+    });
+    
+   /* shop-cart*/
+   
---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,440 @@
+dmx.config({
+  "index": {
+    "repeat1": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "cat_id"
+        },
+        {
+          "type": "text",
+          "name": "en_name"
+        },
+        {
+          "type": "text",
+          "name": "bn_name"
+        },
+        {
+          "type": "text",
+          "name": "hin_name"
+        },
+        {
+          "type": "text",
+          "name": "image"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeatProduct": {
+      "meta": [
+        {
+          "name": "p_id",
+          "type": "number"
+        },
+        {
+          "name": "prod_id",
+          "type": "text"
+        },
+        {
+          "name": "prod_mark_id",
+          "type": "number"
+        },
+        {
+          "name": "prod_name_en",
+          "type": "text"
+        },
+        {
+          "name": "prod_name_bn",
+          "type": "text"
+        },
+        {
+          "name": "prod_name_hin",
+          "type": "text"
+        },
+        {
+          "name": "prod_img1",
+          "type": "text"
+        },
+        {
+          "name": "quantity",
+          "type": "text"
+        },
+        {
+          "name": "q_unit",
+          "type": "text"
+        },
+        {
+          "name": "mrp",
+          "type": "text"
+        },
+        {
+          "name": "price",
+          "type": "text"
+        },
+        {
+          "name": "product_tax",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeatCategory": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "cat_id"
+        },
+        {
+          "type": "text",
+          "name": "en_name"
+        },
+        {
+          "type": "text",
+          "name": "bn_name"
+        },
+        {
+          "type": "text",
+          "name": "hin_name"
+        },
+        {
+          "type": "text",
+          "name": "image"
+        }
+      ],
+      "outputType": "array"
+    },
+    "repeatCat": {
+      "meta": [
+        {
+          "name": "offset",
+          "type": "number"
+        },
+        {
+          "name": "limit",
+          "type": "number"
+        },
+        {
+          "name": "total",
+          "type": "number"
+        },
+        {
+          "name": "page",
+          "type": "object",
+          "sub": [
+            {
+              "name": "offset",
+              "type": "object",
+              "sub": [
+                {
+                  "name": "first",
+                  "type": "number"
+                },
+                {
+                  "name": "prev",
+                  "type": "number"
+                },
+                {
+                  "name": "next",
+                  "type": "number"
+                },
+                {
+                  "name": "last",
+                  "type": "number"
+                }
+              ]
+            },
+            {
+              "name": "current",
+              "type": "number"
+            },
+            {
+              "name": "total",
+              "type": "number"
+            }
+          ]
+        },
+        {
+          "name": "data",
+          "type": "array",
+          "sub": [
+            {
+              "type": "number",
+              "name": "id"
+            },
+            {
+              "type": "text",
+              "name": "en_name"
+            },
+            {
+              "type": "text",
+              "name": "bn_name"
+            },
+            {
+              "type": "text",
+              "name": "hin_name"
+            },
+            {
+              "type": "text",
+              "name": "image"
+            }
+          ]
+        }
+      ],
+      "outputType": "object"
+    },
+    "repeatProd": {
+      "meta": [
+        {
+          "name": "p_id",
+          "type": "number"
+        },
+        {
+          "name": "prod_id",
+          "type": "text"
+        },
+        {
+          "name": "prod_mark_id",
+          "type": "number"
+        },
+        {
+          "name": "prod_name_en",
+          "type": "text"
+        },
+        {
+          "name": "prod_name_bn",
+          "type": "text"
+        },
+        {
+          "name": "prod_name_hin",
+          "type": "text"
+        },
+        {
+          "name": "prod_img1",
+          "type": "text"
+        },
+        {
+          "name": "quantity",
+          "type": "text"
+        },
+        {
+          "name": "q_unit",
+          "type": "text"
+        },
+        {
+          "name": "mrp",
+          "type": "text"
+        },
+        {
+          "name": "price",
+          "type": "text"
+        },
+        {
+          "name": "product_tax",
+          "type": "text"
+        }
+      ],
+      "outputType": "array"
+    }
+  },
+  "main": {
+    "repeatlanguage": {
+      "meta": [
+        {
+          "name": "$index",
+          "type": "number"
+        },
+        {
+          "name": "$key",
+          "type": "text"
+        },
+        {
+          "name": "$value",
+          "type": "object"
+        },
+        {
+          "type": "text",
+          "name": "language_name"
+        },
+        {
+          "type": "text",
+          "name": "language_code"
+        }
+      ],
+      "outputType": "array"
+    },
+    "varLanguage": {
+      "meta": [],
+      "outputType": "text"
+    }
+  },
+  "cart": {
+    "repeatcart": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "number",
+          "name": "prod_id"
+        },
+        {
+          "type": "number",
+          "name": "price_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "prod_unit_price"
+        },
+        {
+          "type": "text",
+          "name": "prod_qty"
+        },
+        {
+          "type": "text",
+          "name": "total_price"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_en"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_bn"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_hin"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_en"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_bn"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_hin"
+        },
+        {
+          "type": "text",
+          "name": "prod_img1"
+        },
+        {
+          "type": "text",
+          "name": "quantity"
+        },
+        {
+          "type": "text",
+          "name": "q_unit"
+        },
+        {
+          "type": "text",
+          "name": "mrp"
+        },
+        {
+          "type": "text",
+          "name": "price"
+        },
+        {
+          "type": "text",
+          "name": "product_tax"
+        }
+      ],
+      "outputType": "array"
+    }
+  },
+  "wishlist": {
+    "repeatProd": {
+      "meta": [
+        {
+          "type": "number",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "prod_id"
+        },
+        {
+          "type": "number",
+          "name": "price_id"
+        },
+        {
+          "type": "number",
+          "name": "user_id"
+        },
+        {
+          "type": "text",
+          "name": "prod_unit_price"
+        },
+        {
+          "type": "text",
+          "name": "prod_qty"
+        },
+        {
+          "type": "text",
+          "name": "total_price"
+        },
+        {
+          "type": "text",
+          "name": "ips"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_en"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_bn"
+        },
+        {
+          "type": "text",
+          "name": "prod_name_hin"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_en"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_bn"
+        },
+        {
+          "type": "text",
+          "name": "prod_desc_hin"
+        },
+        {
+          "type": "text",
+          "name": "prod_img1"
+        },
+        {
+          "type": "text",
+          "name": "quantity"
+        },
+        {
+          "type": "text",
+          "name": "q_unit"
+        },
+        {
+          "type": "text",
+          "name": "mrp"
+        },
+        {
+          "type": "text",
+          "name": "price"
+        },
+        {
+          "type": "text",
+          "name": "product_tax"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});

3d7868dce600e38fc2d269c6b04432475170e2eb
DESKTOP-34ARMFG\shivalik <harshitmaheshwari265@gmail.com>
2023-01-05 13:07:47 +0545

    --update main modal

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -224,13 +224,13 @@
 })(jQuery);
 /*cart*/
 // Get the modal
-var modal = document.getElementById("myModal");
+//var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+//var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+//var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
 // btn.onclick = function () {
@@ -242,28 +242,28 @@ var span = document.getElementsByClassName("close")[0];
 // }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-const loginText = document.querySelector(".title-text .login");
-const loginForm = document.querySelector("form.login");
-const loginBtn = document.querySelector("label.login");
-const signupBtn = document.querySelector("label.signup");
-const signupLink = document.querySelector("form .signup-link a");
-signupBtn.onclick = (() => {
-    loginForm.style.marginLeft = "-50%";
-    loginText.style.marginLeft = "-50%";
-});
-loginBtn.onclick = (() => {
-    loginForm.style.marginLeft = "0%";
-    loginText.style.marginLeft = "0%";
-});
-signupLink.onclick = (() => {
-    signupBtn.click();
-    return false;
-});
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+// const loginText = document.querySelector(".title-text .login");
+// const loginForm = document.querySelector("form.login");
+// const loginBtn = document.querySelector("label.login");
+// const signupBtn = document.querySelector("label.signup");
+// const signupLink = document.querySelector("form .signup-link a");
+// signupBtn.onclick = (() => {
+//     loginForm.style.marginLeft = "-50%";
+//     loginText.style.marginLeft = "-50%";
+// });
+// loginBtn.onclick = (() => {
+//     loginForm.style.marginLeft = "0%";
+//     loginText.style.marginLeft = "0%";
+// });
+// signupLink.onclick = (() => {
+//     signupBtn.click();
+//     return false;
+// });
 
 // function myFunction() {
 //     var input, filter, table, tr, td, i, txtValue;
